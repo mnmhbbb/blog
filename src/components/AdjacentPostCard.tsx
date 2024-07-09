8ddcf4476ec4329f81035b295280a74c565adad6
mnmhbbb <mnmhb64@gmail.com>
2024-07-10 00:25:39 +0900

    remove unused code

--- src/components/AdjacentPostCard.tsx
+++ src/components/AdjacentPostCard.tsx
@@ -1,5 +1,4 @@
 import { Post } from '@/service/posts';
-import Image from 'next/image';
 import Link from 'next/link';
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 
@@ -10,7 +9,7 @@ type Props = {
 
 const ICON_CLASS = 'text-3xl m-4 text-yellow-300 transition-all';
 
-export default function AdjacentPostCard({ post: { path, title, description }, type }: Props) {
+export default function AdjacentPostCard({ post: { path }, type }: Props) {
   return (
     <Link href={`/posts/${path}`} className='w-full bg-black max-h-56 p-5'>
       <div className='w-full flex justify-around items-center text-white px-8'>
